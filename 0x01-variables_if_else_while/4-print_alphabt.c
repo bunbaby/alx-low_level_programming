08e4d29c38ed91b9fe5bd11296d05ac9a8803a8a
bunbaby <awomolooluwabunmi@gmail.com>
2022-07-01 13:38:32 -0700

    new

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,8 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	putchar('\n');
 
 	return (0);
