18e6f785bdebc3f6c1b74df606cd400ff992ece8
bunbaby <awomolooluwabunmi@gmail.com>
2022-07-07 01:22:57 -0700

    new

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ *main - Write a function that checks for uppercase character
+ *Return: 1 if c is upper case
+ *0 if otherwise
+ *@c: the given letter
+ */
+
+int _isupper(int c);
+{
+	char c;
+
+	if c >= 'A' && c <= 'Z'
+		return (1);
+	return (0);
