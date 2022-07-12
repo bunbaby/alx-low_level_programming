26088dcec05f3d7e6461af41c69ffdaeb6706fc4
bunbaby <awomolooluwabunmi@gmail.com>
2022-07-12 10:14:34 -0700

    new

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,13 @@
 
 void print_rev(char *s)
 {
-	printf("%s\n", strrev(s);
+	int len = 0, index;
 
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
 }
