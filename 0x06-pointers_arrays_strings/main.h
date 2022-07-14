9311660af4b08b40d38161eaca4d713456ac897e
bunbaby <awomolooluwabunmi@gmail.com>
2022-07-14 09:47:37 -0700

    new

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,9 @@ int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 char *string_toupper(char *s);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
