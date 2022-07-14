786e804a6494c7079af6956e32053093401320f5
bunbaby <awomolooluwabunmi@gmail.com>
2022-07-14 06:22:46 -0700

    new

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,17 +10,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char **p;
-	int i, j;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		dest[i] = dest[i];
-	}
-	for (j = 0; src[i] != '\0' && j < n; j++)
-	{
-		dest[i] = src[j];
-	}
-	p = &dest;
-	return (*p);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
